1f44aa08297a8e985fa767e88484deab5c34cad5
Subramanian Chockalingam <subramanianchockalingam@subramanians-mac-mini.local>
2018-10-22 22:56:00 +0530

    Initial Commit

---
+++ MultiSelectDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiSelectDemo
+//
+//  Created by Subramanian Chockalingam on 22/10/18.
+//  Copyright © 2018 Subramanian Chockalingam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
